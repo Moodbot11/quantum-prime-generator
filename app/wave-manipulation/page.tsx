c3398645c5b320b930cb807dd9b183888858784e
Moodbot11 <enigmaitech1@gmail.com>
2024-12-23 21:11:34 -0500

    second commit

--- app/wave-manipulation/page.tsx
+++ app/wave-manipulation/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useRef, useCallback } from 'react'
+import React, { useState, useRef, useCallback, useEffect } from 'react'
 import Link from 'next/link'
 
 const WaveManipulation: React.FC = () => {
