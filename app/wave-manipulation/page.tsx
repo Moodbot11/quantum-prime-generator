e9af687fa279a14af7b0c946abe70d0f86c7a68b
Moodbot11 <enigmaitech1@gmail.com>
2024-12-25 02:08:04 -0500

    second commit

--- app/wave-manipulation/page.tsx
+++ app/wave-manipulation/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React, { useState, useRef, useEffect } from 'react'
-import WaveManipulation from '../../components/WaveManipulation'
-import GoogleAIChatbot from '../../components/GoogleAIChatbot'
+import WaveManipulation from '@/components/WaveManipulation'
+import GoogleAIChatbot from '@/components/GoogleAIChatbot'
 import { Button } from "@/components/ui/button"
 
 export default function WaveManipulationPage() {
