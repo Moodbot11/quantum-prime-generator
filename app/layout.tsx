6a46eeab3bbfe18fc4491071459bfe74209abebd
MARK MOODY <enigmaitech1@gmail.com>
2025-04-15 16:07:46 -0600

    added analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navigation from '../components/Navigation'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
